f9d000c257ba602afb36a26f301a0de14043369f
antiyro <antiyro@gmail.com>
2025-01-14 00:50:39 +0000

    cleaned for release

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { StarknetAgent } from './lib/agent/starknetAgent';
+export { IAgent } from './agents/interfaces/agent.interface';
+export * from './common/errors';
+export * from './lib/utils/types';
