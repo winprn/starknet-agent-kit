4c03bf5fa23cf94c6e781b532276225a3775ce50
antiyro <antiyro@gmail.com>
2025-02-05 21:22:33 +0000

    feat(release): cleaned for release

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-export { StarknetAgent } from './lib/agent/starknetAgent';
-export { IAgent } from './agents/interfaces/agent.interface';
-export * from './common/errors';
+// Main exports
+export * from './lib/agent/agent';
+export * from './lib/agent/starknetAgent';
+export * from './lib/agent/autonomousAgents';
+
+// Plugin exports
 export * from './lib/agent/plugins/core/account/types/accounts';
+export * from './lib/agent/plugins/core/account/types/deployAccountTypes';
+export * from './lib/agent/plugins/core/contract/types/contract';
+export * from './lib/agent/plugins/core/token/types/balance';
+export * from './lib/agent/plugins/core/transaction/types/estimate';
+export * from './lib/agent/plugins/core/transaction/types/simulateTransactionTypes';
+
+// Utilities
+export * from './lib/agent/tools/tools';
+export * from './lib/agent/tools/external_tools';
+export * from './lib/agent/tools/signatureTools';
